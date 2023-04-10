fe717518ef07a610a0fe1c7888d4ba084734c1de
uskevichj <uskevichj@gmail.com>
2023-04-09 21:24:51 -0400

    Added buttons, created variables.  Starting to rename the buttons logically.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,301 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button0 = new Button();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button17 = new Button();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // button0
+            // 
+            button0.FlatStyle = FlatStyle.System;
+            button0.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button0.Location = new Point(12, 463);
+            button0.Name = "zero";
+            button0.Size = new Size(156, 75);
+            button0.TabIndex = 0;
+            button0.Text = "0";
+            button0.UseVisualStyleBackColor = true;
+            button0.Click += zero_Click;
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.System;
+            button1.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(12, 382);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 75);
+            button1.TabIndex = 1;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += one_Click;
+            // 
+            // button2
+            // 
+            button2.FlatStyle = FlatStyle.System;
+            button2.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(95, 382);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 75);
+            button2.TabIndex = 2;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.FlatStyle = FlatStyle.System;
+            button3.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.Location = new Point(176, 382);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 75);
+            button3.TabIndex = 3;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.FlatStyle = FlatStyle.System;
+            button4.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button4.Location = new Point(12, 301);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 75);
+            button4.TabIndex = 4;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.FlatStyle = FlatStyle.System;
+            button5.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button5.Location = new Point(93, 301);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 75);
+            button5.TabIndex = 5;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.FlatStyle = FlatStyle.System;
+            button6.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button6.Location = new Point(176, 301);
+            button6.Name = "button6";
+            button6.Size = new Size(75, 75);
+            button6.TabIndex = 6;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.FlatStyle = FlatStyle.System;
+            button7.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button7.Location = new Point(12, 220);
+            button7.Name = "button7";
+            button7.Size = new Size(75, 75);
+            button7.TabIndex = 7;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.FlatStyle = FlatStyle.System;
+            button8.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button8.Location = new Point(93, 220);
+            button8.Name = "button8";
+            button8.Size = new Size(75, 75);
+            button8.TabIndex = 8;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.FlatStyle = FlatStyle.System;
+            button9.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button9.Location = new Point(176, 220);
+            button9.Name = "button9";
+            button9.Size = new Size(75, 75);
+            button9.TabIndex = 9;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.FlatStyle = FlatStyle.System;
+            button10.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button10.Location = new Point(176, 463);
+            button10.Name = "button10";
+            button10.Size = new Size(75, 75);
+            button10.TabIndex = 10;
+            button10.Text = ".";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // button11
+            // 
+            button11.FlatStyle = FlatStyle.System;
+            button11.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button11.Location = new Point(257, 139);
+            button11.Name = "button11";
+            button11.Size = new Size(75, 75);
+            button11.TabIndex = 11;
+            button11.Text = "/";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
+            // 
+            // button12
+            // 
+            button12.FlatStyle = FlatStyle.System;
+            button12.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button12.Location = new Point(176, 139);
+            button12.Name = "button12";
+            button12.Size = new Size(75, 75);
+            button12.TabIndex = 12;
+            button12.Text = "*";
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += button12_Click;
+            // 
+            // button13
+            // 
+            button13.FlatStyle = FlatStyle.System;
+            button13.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button13.Location = new Point(93, 139);
+            button13.Name = "button13";
+            button13.Size = new Size(75, 75);
+            button13.TabIndex = 13;
+            button13.Text = "+";
+            button13.UseVisualStyleBackColor = true;
+            button13.Click += button13_Click;
+            // 
+            // button14
+            // 
+            button14.FlatStyle = FlatStyle.System;
+            button14.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button14.Location = new Point(12, 139);
+            button14.Name = "button14";
+            button14.Size = new Size(75, 75);
+            button14.TabIndex = 14;
+            button14.Text = "-";
+            button14.UseVisualStyleBackColor = true;
+            button14.Click += button14_Click;
+            // 
+            // button15
+            // 
+            button15.FlatStyle = FlatStyle.System;
+            button15.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button15.Location = new Point(257, 220);
+            button15.Name = "button15";
+            button15.Size = new Size(75, 75);
+            button15.TabIndex = 15;
+            button15.Text = "<-";
+            button15.UseVisualStyleBackColor = true;
+            button15.Click += button15_Click;
+            // 
+            // button16
+            // 
+            button16.FlatStyle = FlatStyle.System;
+            button16.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button16.Location = new Point(257, 301);
+            button16.Name = "button16";
+            button16.Size = new Size(75, 75);
+            button16.TabIndex = 16;
+            button16.Text = "C";
+            button16.UseVisualStyleBackColor = true;
+            button16.Click += button16_Click;
+            // 
+            // button17
+            // 
+            button17.FlatStyle = FlatStyle.System;
+            button17.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button17.Location = new Point(257, 382);
+            button17.Name = "button17";
+            button17.Size = new Size(75, 156);
+            button17.TabIndex = 17;
+            button17.Text = "=";
+            button17.UseVisualStyleBackColor = true;
+            button17.Click += button17_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(12, 26);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(318, 71);
+            textBox1.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(344, 550);
+            Controls.Add(textBox1);
+            Controls.Add(button17);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(button0);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button0;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+        private TextBox textBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,108 @@ namespace Calc_UI
 {
     public partial class Form1 : Form
     {
+        // Create variables to store the first number, second number and the result
+        string input = string.Empty;
+        string firstNumber = string.Empty;
+        string secondNumber = string.Empty;
+        char operation;
+        double result = 0.0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        // This method will be called when the user clicks on any of the number buttons
+
+        private void zero_Click(object sender, EventArgs e)
+        {
+            input += "0";
+        }
+
+        private void one_Click(object sender, EventArgs e)
+        {
+            input += "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            input += "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            input += "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            input += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            input += "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            input += "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            input += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            input += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            input += "9";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            input += ".";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
